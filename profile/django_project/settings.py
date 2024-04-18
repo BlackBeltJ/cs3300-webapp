400788d6080902778b45beb749c6167758314292
Joshua Edwards <BlackBeltJJE@gmail.com>
2024-04-18 17:48:26 -0600

    fiddling with Selenium testing

--- profile/django_project/settings.py
+++ profile/django_project/settings.py
@@ -145,3 +145,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# selenium driver
+SELENIUM_WEBDRIVER = 'Chrome'
