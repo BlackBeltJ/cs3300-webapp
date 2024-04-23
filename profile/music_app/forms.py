8f6f76a18f3f4408ef1888faf586dd61d0a692d7
Joshua Edwards <BlackBeltJJE@gmail.com>
2024-04-23 11:39:50 -0600

    blew up comments

--- profile/music_app/forms.py
+++ profile/music_app/forms.py
@@ -16,7 +16,6 @@ class ArtistForm(ModelForm):
     class Meta:
         model = Artist
         fields = '__all__'
-        #fields = ['name', 'email', 'genre', 'instrument']
         exclude = ['user', 'profile']
         
 class PostForm(ModelForm):
@@ -24,28 +23,6 @@ class PostForm(ModelForm):
         model = Post
         fields = '__all__'
         exclude = ['profile']
-        # widgets = {
-        #     'mp3_file': forms.FileInput(attrs={'id': 'mp3_file_id'}), # 'audio/mp3
-        # }
-        
-    # def clean_audio_file(self):
-    #     file = self.cleaned_data.get('audio_file',False)
-    #     if file:
-    #         if file._size > 4*1024*1024:
-    #            raise ValidationError("Audio file too large ( > 4mb )")
-    #         if not file.content-type in ["audio/mp3"]:
-    #            raise ValidationError("Content-Type is not mpeg")
-    #         if not os.path.splitext(file.name)[1] in [".mp3"]:
-    #            raise ValidationError("Doesn't have proper extension")
-    #         # Here we need to now to read the file and see if it's actually 
-    #         # a valid audio file. I don't know what the best library is to 
-    #         # to do this
-    #         # if not some_lib.is_audio(file.content):
-    #         #       raise ValidationError("Not a valid audio file")
-    #         return file
-    #     else:
-    #         raise ValidationError("Couldn't read uploaded file")
-    
         
 class CreateUserForm(UserCreationForm):
     class Meta:
--- profile/music_app/tests_folder/selenium_tests/test_selenium.py
+++ profile/music_app/tests_folder/selenium_tests/test_selenium.py
@@ -15,9 +15,11 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.firefox.service import Service as FirefoxService
 
 class HostTest(LiveServerTestCase):
+    # setup function gets called automatically before every test
     def setUp(self):
         options = webdriver.ChromeOptions()
         self.browser = webdriver.Chrome(options=options)
+        # browser settings
         self.browser.implicitly_wait(10)
         self.browser.get(self.live_server_url)
         self.browser.maximize_window()
@@ -26,25 +28,23 @@ class HostTest(LiveServerTestCase):
         self.browser.quit()
 
     def test_home_page(self):
-        # options = webdriver.ChromeOptions()
-        # chrome_browser = webdriver.Chrome(options=options)
-        
+        # get home page
         self.browser.get('http://localhost:8000/')
-        #self.browser.get(reverse('index'))
-        #time.sleep(5)
         wait = WebDriverWait(self.browser, 10)
+        # wait until page is fully loaded
         wait.until(lambda condition: self.browser.current_url == 'http://localhost:8000/' or 'http://localhost:8000/accounts/login/?next=/')
-        
         assert 'Artist' in self.browser.title
         
     def test_login_page(self):
+        # get login page
         self.browser.get('http://localhost:8000/accounts/login')
         wait = WebDriverWait(self.browser, 10)
+        # wait until page is fully loaded
         wait.until(lambda condition: self.browser.current_url == 'http://localhost:8000/accounts/login' or 'http://localhost:8000/accounts/login/?next=/')
-        
         assert 'Register' in self.browser.page_source
 
 class LoginFormTest(LiveServerTestCase):
+    # setup function gets called automatically before every test
     def setUp(self):
         options = webdriver.ChromeOptions()
         self.browser = webdriver.Chrome(options=options)
@@ -54,9 +54,7 @@ class LoginFormTest(LiveServerTestCase):
         self.browser.maximize_window()
         
     def test_login_form(self):
-        """
-        Django Admin login test
-        """
+        # get login page
         self.browser.get('http://localhost:8000/accounts/login')
 
         # Fill login information of the user
@@ -64,19 +62,20 @@ class LoginFormTest(LiveServerTestCase):
         password = self.browser.find_element(By.NAME, 'password')
         submit = self.browser.find_element(By.NAME, 'submit')
         
+        # the following lines will enter in the login information
+        # wait a second before entering login info
         time.sleep(1)
         username.send_keys('testUser')
         password.send_keys('testpassword')
-        
-        #time.sleep(2)
         wait = WebDriverWait(self.browser, 5)
         submit.send_keys(Keys.RETURN)
 
-        #time.sleep(2)
+        # wait until page is fully loaded
         wait.until(lambda condition: self.browser.current_url != 'http://localhost:8000/accounts/login/?next=/accounts/login/')
         assert 'testUser' in self.browser.page_source
 
 class YourProfilePageTest(LiveServerTestCase):  
+    # setup function gets called automatically before every test
     def setUp(self):
         options = webdriver.ChromeOptions()
         self.browser = webdriver.Chrome(options=options)
@@ -84,6 +83,7 @@ class YourProfilePageTest(LiveServerTestCase):
         self.browser.set_page_load_timeout(5)
         self.browser.get(self.live_server_url)
         self.browser.maximize_window()
+        
         # first login
         self.browser.get('http://localhost:8000/accounts/login')
 
@@ -105,30 +105,32 @@ class YourProfilePageTest(LiveServerTestCase):
         
         # go to your profile  
         wait = WebDriverWait(self.browser, 5)     
+        # wait until page is fully loaded
         wait.until(lambda condition: self.browser.current_url != 'http://localhost:8000/accounts/login/')
         yourProfile = self.browser.find_element(By.NAME, 'yourProfile')
         yourProfile.send_keys(Keys.RETURN)
         
+        # wait until page is fully loaded
         wait.until(lambda condition: self.browser.current_url != 'http://localhost:8000/')
         assert 'test1' in self.browser.page_source # 'test1' is testUser's profile name, checking that the profile page is correct
 
     def test_artist_page(self):
         self.browser.get('http://localhost:8000/artists/')
         wait = WebDriverWait(self.browser, 5)
+        # wait until page is fully loaded
         wait.until(lambda condition: self.browser.current_url == 'http://localhost:8000/artists/')
-        
         assert 'List of artists' in self.browser.page_source
         
     def test_profile_page(self):
         self.browser.get('http://localhost:8000/artists/37/profile')
         wait = WebDriverWait(self.browser, 5)
+        # wait until page is fully loaded
         wait.until(lambda condition: self.browser.current_url == 'http://localhost:8000/artists/37/profile')
-        
         assert 'test1' in self.browser.page_source
         
     def test_post_page(self):
         self.browser.get('http://localhost:8000/artist/37/profile/create_post')
         wait = WebDriverWait(self.browser, 5)
+        # wait until page is fully loaded
         wait.until(lambda condition: self.browser.current_url == 'http://localhost:8000/artist/37/profile/create_post')
-
         assert 'Create new post' in self.browser.page_source
